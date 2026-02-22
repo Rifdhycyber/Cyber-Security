12bf9ea8eeee18c2241fe3d01f52782cbbe3cb0b
Rifdhy <rifcyberart@gmail.com>
2026-02-22 17:58:46 +0530

    if elsepractice

---
+++ if else practice/ifelse1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+
+    int temp;
+
+    printf("enter ur climate temprature : ");
+    scanf("%d" , &temp);
+    if (temp > 25)
+    {
+        printf("go to the play ground");
+    }
+    else{
+        printf("go to the library");
+    }
+    
+}
