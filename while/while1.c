332b8d88e4e2ef80d20261064579785f02077566
Rifdhy <rifcyberart@gmail.com>
2026-02-26 19:40:27 +0530

    while loop practice

---
+++ while/while1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+    char i = 0;
+
+    while (i <= 122)
+    {
+        printf("%c = %d \n", i, i);
+        i++;
+    }
+    
+    return 0;
+    
+}
---
+++ while/while2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+    int countdown = 10;
+
+    while (countdown > 0)
+    {
+        printf("%d \n", countdown);
+        countdown--;
+    }
+
+    printf("Happ new year everyone");
+
+
+}
