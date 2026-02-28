642cb0ad2985954688c7f252f462bed10ea97222
Rifdhy <rifcyberart@gmail.com>
2026-02-28 17:36:28 +0530

    user input

---
+++ user input/userinput.c
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ int main() {
+    int mynum;
+    float fnum;
+    char myletter;
+
+    printf("type a letter u want : ");
+    scanf("%c", &myletter);
+    printf("your letter is %c \n", myletter);
+
+    printf("type a number u want : ");
+    scanf("%d", &mynum);
+    printf("your number is %d \n ", mynum);
+
+    printf("type a float number u want : ");
+    scanf("%f", &fnum);
+    printf("your float number is %f \n", fnum);
+
+    
+}
